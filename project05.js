5e81997db755837e135329403359f844cc109950
leemeg <marshalll@student.ncmich.edu>
2018-11-03 15:12:24 -0400

    basics work

--- project05.js
+++ project05.js
@@ -19,12 +19,12 @@ const ROCK = 4;
 const OTHER = 5;
 const GENRE = 0;
 const PRICE = 1;
-const GENRE_STRING = 2;
 const TOTAL_PURCH = 0;
 const OVER_TEN = 1;
 const OVER_SIX = 2;
 const OVER_THREE = 3;
 const UNDER_THREE = 4;
+const GENRE_STRING = 5;
 
 let tempGenre, price;
 
@@ -42,14 +42,12 @@ transStats[OTHER] = [0,0,0,0,0,"Other"];
 
 
 function main() {
-    while (0 === 0)
-    {
-        transGenorator();
+    transGenorator();//generates data for prog testing, would not be in actual prog
+    while (0 === 0) {//continuous loop
         settransInfo();
-        /*sortGenre();
-        console.log(transStats);
+
         sortTransOrder();
-        console.log(transStats);*/
+        dispStatsReport();
     }
 }
 main();
@@ -60,12 +58,12 @@ function settransInfo(){
     const PMIN = 0;
     const PMAX = 15.00;
     console.log(`Please enter value for the genre of purchase.`);
-    console.log(`[1] ${transStats[0][5]}`);
-    console.log(`[2] ${transStats[1][5]}`);
-    console.log(`[3] ${transStats[2][5]}`);
-    console.log(`[4] ${transStats[3][5]}`);
-    console.log(`[5] ${transStats[4][5]}`);
-    console.log(`[6] ${transStats[5][5]}`);
+    console.log(`[1] ${transStats[CLASSICAL][GENRE_STRING]}`);
+    console.log(`[2] ${transStats[EASY_LISTEN][GENRE_STRING]}`);
+    console.log(`[3] ${transStats[JAZZ][GENRE_STRING]}`);
+    console.log(`[4] ${transStats[POP][GENRE_STRING]}`);
+    console.log(`[5] ${transStats[ROCK][GENRE_STRING]}`);
+    console.log(`[6] ${transStats[OTHER][GENRE_STRING]}`);
 
     let i = 0;
     if (transaction[i][GENRE] === undefined){
@@ -94,19 +92,12 @@ function settransInfo(){
         }
     }
     transaction[i][PRICE] = price;
-
-
-
-
-
-    console.log(transaction);
-
-
+    sortGenre(i);
 
 }
 
 function transGenorator() {
-    const NUMGEN = 10;
+    const NUMGEN = 1000;
 
     for (let i = 0; i < NUMGEN; i++) {
         transaction[i] = [];
@@ -114,14 +105,14 @@ function transGenorator() {
         price = Math.floor((Math.random() * 1500)+1);
         price = (price * .01).toFixed(2);
         transaction[i][PRICE] = Number(price);
+        sortGenre(i);
     }
+
+
 
 }
 
-p[=
-    0.032654987*/unction sortGenre() {
-
-    for (let i = 0; i < transaction.length; i++){
+function sortGenre(i) {
         switch (transaction[i][GENRE]) {
             case 0:
                 transStats[CLASSICAL][TOTAL_PURCH]++;
@@ -154,9 +145,6 @@ p[=
                 sortPrice(tempGenre, i);
 
         }
-
-    }
-
 }
 
 function sortPrice(tempGenre, i) {
@@ -179,16 +167,28 @@ function sortPrice(tempGenre, i) {
 }
 
 function sortTransOrder() {
-    let unsort = transStats.length;
+    let reorderStats = transStats;
+    let unsort = reorderStats.length;
     for (let y = 0; y < unsort; y++) {
         for (let x = 0; x < unsort; x++){
-            if (transStats[x + 1] > transStats[x]){
-                let temp = transStats[x + 1];
-                transStats[x + 1] = transStats[x];
-                transStats[x] = temp;
+            if (reorderStats[x + 1] > reorderStats[x]){
+                let temp = reorderStats[x + 1];
+                reorderStats[x + 1] = reorderStats[x];
+                reorderStats[x] = temp;
             }
         }
 
     }
+    console.log(reorderStats);
+}
 
+function dispStatsReport() {
+    console.log(`    Genre        Over $10    $6.00-$9.99   $3.00-$5.99   Under $3.00`);
+    console.log(`-------------    --------    -----------   -----------   -----------`);
+    console.log(`  ${transStats[CLASSICAL][GENRE_STRING]}         ${transStats[CLASSICAL][OVER_TEN]}            ${transStats[CLASSICAL][OVER_SIX]}            ${transStats[CLASSICAL][OVER_THREE]}           ${transStats[CLASSICAL][UNDER_THREE]}`);
+    console.log(`${transStats[EASY_LISTEN][GENRE_STRING]}      ${transStats[EASY_LISTEN][OVER_TEN]}            ${transStats[EASY_LISTEN][OVER_SIX]}            ${transStats[EASY_LISTEN][OVER_THREE]}           ${transStats[EASY_LISTEN][UNDER_THREE]}`);
+    console.log(`     ${transStats[JAZZ][GENRE_STRING]}           ${transStats[JAZZ][OVER_TEN]}            ${transStats[JAZZ][OVER_SIX]}            ${transStats[JAZZ][OVER_THREE]}           ${transStats[JAZZ][UNDER_THREE]}`);
+    console.log(`     ${transStats[POP][GENRE_STRING]}            ${transStats[POP][OVER_TEN]}            ${transStats[POP][OVER_SIX]}            ${transStats[POP][OVER_THREE]}           ${transStats[POP][UNDER_THREE]}`);
+    console.log(`     ${transStats[ROCK][GENRE_STRING]}           ${transStats[ROCK][OVER_TEN]}            ${transStats[ROCK][OVER_SIX]}            ${transStats[ROCK][OVER_THREE]}           ${transStats[ROCK][UNDER_THREE]}`);
+    console.log(`     ${transStats[OTHER][GENRE_STRING]}          ${transStats[OTHER][OVER_TEN]}            ${transStats[OTHER][OVER_SIX]}            ${transStats[OTHER][OVER_THREE]}           ${transStats[OTHER][UNDER_THREE]}`);
 }
